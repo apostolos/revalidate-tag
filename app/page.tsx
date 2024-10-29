5362f40a1880d6f575427ea2d336460e0d1a8102
Apostolos Tsakpinis <apostolos.tsakpinis@gmail.com>
2024-10-29 23:06:54 +0000

    reduce more

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,32 @@
-import { Suspense } from 'react';
-import { unstable_cacheTag as cacheTag, revalidateTag } from 'next/cache';
+import { Suspense } from "react";
+import { unstable_cacheTag as cacheTag, revalidateTag } from "next/cache";
+
+async function revalidate() {
+  "use server";
+  revalidateTag("mytag");
+}
+
+async function Comp() {
+  "use cache";
+  cacheTag("mytag");
+  const data = await fetch(
+    "https://next-data-api-endpoint.vercel.app/api/random"
+  );
+  const result = await data.text();
+  return <span>{result}</span>;
+}
 
 export default function Home() {
   return (
     <>
       <Suspense fallback={<p>Loading...</p>}>
         <p>
-          Not Revalidating: <CompA />
-        </p>
-        <p>
-          Revalidating: <CompB />
+          Not Revalidating: <Comp />
         </p>
       </Suspense>
-      <form action={invalidate}>
+      <form action={revalidate}>
         <button>Revalidate</button>
       </form>
     </>
   );
-}
-
-async function invalidate() {
-  'use server';
-  revalidateTag('mytag');
-}
-
-async function CompA() {
-  'use cache';
-  cacheTag('mytag');
-  const data = await fetch('https://next-data-api-endpoint.vercel.app/api/random');
-  const result = await data.text();
-  return <span>{result}</span>;
-}
-
-async function CompB() {
-  'use cache';
-  cacheTag('mytag');
-  const data = await fetch('https://next-data-api-endpoint.vercel.app/api/random', {
-    headers: {
-      'x-bust': Math.random().toString(),
-    },
-  });
-  const result = await data.text();
-  return <span>{result}</span>;
 }
